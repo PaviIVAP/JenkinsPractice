3aaee68fcf6c3281645511a1341f8ee3014d6820
PaviIVAP <110247491+PaviIVAP@users.noreply.github.com>
2025-06-08 14:06:48 +0530

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(4, calc.add(2, 2));
+    }
+}
